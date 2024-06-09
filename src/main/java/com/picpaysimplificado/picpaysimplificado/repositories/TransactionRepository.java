323e25d46509a766e34288e4117abe4cc264c0e2
Daniel Benício <danielbenicio123@gmail.com>
2024-06-09 19:07:04 -0300

    feat: update User domain

--- src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
@@ -3,6 +3,4 @@ package com.picpaysimplificado.picpaysimplificado.repositories;
 import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> { }
