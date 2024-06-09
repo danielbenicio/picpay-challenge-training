103bfeb4d8c020ae5fb0968312f6f6f46a64d633
Daniel Benício <danielbenicio123@gmail.com>
2024-06-09 19:06:27 -0300

    feat: create Transaction Domain

--- src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -2,10 +2,7 @@ package com.picpaysimplificado.picpaysimplificado.domain.transaction;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -28,6 +25,7 @@ public class Transaction {
     @JoinColumn(name="receiver_id")
     private User receiver;
     private LocalDateTime timestamp;
+
     public Transaction() {
 
     }
